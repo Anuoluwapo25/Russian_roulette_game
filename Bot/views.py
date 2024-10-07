52e12d08ecfb40c014cd031cb12c5a9885df20f3
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-10-07 18:52:32 +0100

    added gunicon

--- Bot/views.py
+++ Bot/views.py
@@ -9,6 +9,10 @@ from django.views.decorators.http import require_POST
 
 from .bot import bot_application
 
+def home(request):
+    return HttpResponse("Welcome to the Telegram Bot!")
+
+
 @csrf_exempt
 def telegram_webhook(request):
     if request.method == "POST":
