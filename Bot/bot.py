b788a858b1a319a032c40e9d21ce5cd8cd70e3ce
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-10-20 15:20:02 +0100

    async for webhook

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,18 +1,10 @@
-import os
-import logging
 from telegram import Update, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import CommandHandler, ContextTypes, Application
+import os
 
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+application = Application.builder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
 
-bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
-if not bot_token:
-    logger.error("TELEGRAM_BOT_TOKEN is not set in the environment.")
-    exit(1)
-
-application = Application.builder().token(bot_token).build()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     web_app = WebAppInfo(url="https://russian-roullette-4taj.vercel.app/")
@@ -23,17 +15,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
     )
 
+
 application.add_handler(CommandHandler("start", start))
 
-# application.run_polling(poll_interval=1)
+# application.run_polling()
+application.run_webhook(
+    listen="0.0.0.0",
+    port=int(os.getenv('PORT', 8443)),
+    url_path=os.getenv('TELEGRAM_BOT_TOKEN'),  
+    webhook_url=f"https://russian-roulette-game.onrender.com/webhook/{os.getenv('TELEGRAM_BOT_TOKEN')}"
+)
 
-async def run_bot():
-    logger.info("Starting the bot with webhook...")
-    
-    await application.run_webhook(
-        listen="0.0.0.0",
-        port=int(os.getenv("PORT", 8443)),
-        url_path="webhook", 
-        webhook_url="https://russian-roulette-game.onrender.com/webhook/"  
-    )
+
+
+
+
+
 
